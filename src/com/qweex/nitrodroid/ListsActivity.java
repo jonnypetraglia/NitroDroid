9212716cc5aba5d69ff84f5be83a203848b2e6a8
MrQweex <mrqweex@qweex.com>
2012-12-03 02:07:58 -0700

    Added a statusbar to the List screen. Yay consistency!

--- src/com/qweex/nitrodroid/ListsActivity.java
+++ src/com/qweex/nitrodroid/ListsActivity.java
@@ -13,6 +13,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
@@ -26,6 +27,7 @@ public class ListsActivity extends ListActivity
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.lists);
 	    try {
 	    	InputStream input = getAssets().open("nitro_data.json");
