b7a19f0bb632437d2bf4fd9adf370c33a4e3cd1d
notbryant <notbryant@gmail.com>
2014-01-23 06:03:36 -0700

    Updated Copyright

--- src/com/qweex/utils/QweexUtils.java
+++ src/com/qweex/utils/QweexUtils.java
@@ -2,7 +2,7 @@
         DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
                     Version 2, December 2004
 
- Copyright (C) 2013 Jon Petraglia <MrQweex@qweex.com>
+ Copyright (C) 2013-2014 Jon Petraglia <MrQweex@qweex.com>
 
  Everyone is permitted to copy and distribute verbatim or modified
  copies of this license document, and changing it is allowed as long
